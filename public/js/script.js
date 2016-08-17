344c79a2c5802f3a662f455047f2416e83637efa
Annette Perez <annechristine99biz@gmail.com>
2016-08-16 20:37:53 -0400

    update

--- public/js/script.js
+++ public/js/script.js
@@ -6,7 +6,7 @@ $(function(){
     console.log('requesting: '+url)
     $.ajax({
       url:url,
-      method:"GET",
+      method:"POST",
       data: {data:original_text},
       success:function(data){
 
